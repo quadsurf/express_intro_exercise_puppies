784b295de8a813c677c568807ca7a9ece2ed3d97
Matt Lane <lane.matthew@gmail.com>
2015-09-01 16:37:25 -0700

    can create new puppies

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express'),
-  app = express();
+  app = express(),
+  bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended: true})); // Comment this out and input fields are undefined
 
 puppies = [
   {name: "Whiskey", age: 2, id: 1}
@@ -13,10 +15,27 @@ app.get('/', function(req, res) {
   res.render('site/index', {puppies: puppies});
 });
 
+app.get('/puppies', function(req, res) {
+  res.render('site/index', {puppies: puppies});
+});
+
 app.get('/puppies/new', function(req, res) {
   res.render('puppies/new');
+  console.log("new page loaded");
+});
+
+app.post('/puppies', function(req, res) {
+  var newPuppy = req.body;
+  puppies.push({
+    name: newPuppy.name,
+    age: newPuppy.age,
+    id: nextPuppyId
+  });
+  nextPuppyId++;
+  res.render('site/index', {puppies: puppies});
 });
 
 app.listen(3000, function () {
   console.log("Starting a server on localhost:3000");
 });
+
