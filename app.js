a3b788376320c78f08461cc8a892f025b85e3fa7
Matt Lane <lane.matthew@gmail.com>
2015-09-01 13:15:40 -0700

    add stylesheet, new page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express'),
   app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 puppies = [
   {name: "Whiskey", age: 2, id: 1}
@@ -10,6 +11,10 @@ nextPuppyId = 2
 
 app.get('/', function(req, res) {
   res.render('site/index', {puppies: puppies});
+});
+
+app.get('/puppies/new', function(req, res) {
+  res.render('puppies/new');
 });
 
 app.listen(3000, function () {
