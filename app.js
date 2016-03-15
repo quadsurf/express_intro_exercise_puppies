0ac8b4c4c4b17c05adc649c30a9d5f86fe23f9fd
Matt Lane <lane.matthew@gmail.com>
2016-03-15 10:25:52 -0700

    add app.js solution

--- app.js
+++ app.js
@@ -1,3 +1,60 @@
-var app;
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+var puppies = [
+  {name: "Whiskey", age: 2, id: 1}
+];
+var nextPuppyId = 2;
+
+function findPuppyById(id) {
+  return puppies.filter(function(puppy) {
+    return puppy.id === id;
+  });
+}
+
+app.get('/', function(req, res) {
+  res.render('index', {puppies: puppies});
+});
+
+app.get('/puppies/new', function(req, res) {
+  res.render('puppies/new');
+});
+
+app.get('/puppies', function(req, res) {
+  var newPuppy = req.query;
+  if (newPuppy.name) {
+    puppies.push({
+      name: newPuppy.name,
+      age: newPuppy.age,
+      id: nextPuppyId
+    });
+    nextPuppyId++;
+  }
+  res.redirect('/');
+});
+
+app.get('/puppies/:id', function(req, res) {
+  var currentPuppy = findPuppyById(parseInt(req.params.id))[0];
+  if (currentPuppy) {
+    res.render('puppies/show', {puppy: currentPuppy});
+  } else {
+    res.render('site/404');
+  }
+});
+
+app.get('/about', function(req, res) {
+  res.render('site/about');
+});
+
+app.get('/contact', function(req, res) {
+  res.render('site/contact');
+});
+
+app.listen(3000, function () {
+  console.log("Starting a server on localhost:3000");
+});
 
 module.exports = app;
